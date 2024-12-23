939b79a33e4697a5412cee4103af01af7d841fa3
Marcelo Amador <marcelo@cklabs.dev>
2024-12-23 18:19:48 -0300

    Começando projeto web

---
+++ solution/src/VelozientComputers.Web/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,45 @@
+﻿namespace VelozientComputers.Web.Services.Interfaces
+{
+    /// <summary>
+    /// Base interface for all HTTP service operations
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    /// <typeparam name="TCreateDTO">DTO type for creation</typeparam>
+    /// <typeparam name="TUpdateDTO">DTO type for updates</typeparam>
+    public interface IBaseService<TEntity, TCreateDTO, TUpdateDTO>
+    {
+        /// <summary>
+        /// Gets all entities
+        /// </summary>
+        /// <returns>Collection of entities</returns>
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// Gets an entity by its identifier
+        /// </summary>
+        /// <param name="id">Entity identifier</param>
+        /// <returns>Entity if found</returns>
+        Task<TEntity> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Creates a new entity
+        /// </summary>
+        /// <param name="createDto">DTO with creation data</param>
+        /// <returns>Created entity</returns>
+        Task<TEntity> CreateAsync(TCreateDTO createDto);
+
+        /// <summary>
+        /// Updates an existing entity
+        /// </summary>
+        /// <param name="id">Entity identifier</param>
+        /// <param name="updateDto">DTO with update data</param>
+        /// <returns>Updated entity</returns>
+        Task<TEntity> UpdateAsync(int id, TUpdateDTO updateDto);
+
+        /// <summary>
+        /// Deletes an entity
+        /// </summary>
+        /// <param name="id">Entity identifier</param>
+        Task DeleteAsync(int id);
+    }
+}
---
+++ solution/src/VelozientComputers.Web/Services/Interfaces/IComputerService.cs
@@ -0,0 +1,10 @@
+﻿namespace VelozientComputers.Web.Services.Interfaces
+{
+    /// <summary>
+    /// Interface for computer-related HTTP operations
+    /// </summary>
+    public interface IComputerService
+    {
+
+    }
+}
