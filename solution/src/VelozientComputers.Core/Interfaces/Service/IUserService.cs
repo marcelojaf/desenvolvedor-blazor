7eaa7d468b69a09dc9e556296b862293b6456259
Marcelo Amador <marcelo@cklabs.dev>
2024-12-27 13:47:25 -0300

    Atualizando

--- solution/src/VelozientComputers.Core/Interfaces/Service/IUserService.cs
+++ solution/src/VelozientComputers.Core/Interfaces/Service/IUserService.cs
@@ -22,38 +22,6 @@ namespace VelozientComputers.Core.Interfaces.Service
         /// <returns>User details if found, null otherwise</returns>
         Task<User> GetUserByIdAsync(int id);
 
-        /// <summary>
-        /// Gets a user by their email address
-        /// </summary>
-        /// <param name="email">Email address</param>
-        /// <returns>User details if found, null otherwise</returns>
-        Task<User> GetUserByEmailAsync(string email);
-
-        #endregion
-
-        #region Command Methods
-
-        /// <summary>
-        /// Creates a new user
-        /// </summary>
-        /// <param name="user">User creation data</param>
-        /// <returns>Created user details</returns>
-        Task<User> CreateUserAsync(User user);
-
-        /// <summary>
-        /// Updates an existing user
-        /// </summary>
-        /// <param name="id">User identifier</param>
-        /// <param name="user">User update data</param>
-        /// <returns>Updated user details</returns>
-        Task<User> UpdateUserAsync(int id, User user);
-
-        /// <summary>
-        /// Deletes a user
-        /// </summary>
-        /// <param name="id">User identifier</param>
-        Task DeleteUserAsync(int id);
-
         #endregion
     }
 }
--- solution/src/VelozientComputers.Core/Services/UserService.cs
+++ solution/src/VelozientComputers.Core/Services/UserService.cs
@@ -42,65 +42,6 @@ namespace VelozientComputers.Core.Services
             return await _userRepository.GetWithCurrentComputersAsync(id);
         }
 
-        /// <inheritdoc/>
-        public async Task<User> GetUserByEmailAsync(string email)
-        {
-            var users = await _userRepository.FindAsync(u => u.EmailAddress == email);
-            return users.FirstOrDefault();
-        }
-
-        #endregion
-
-        #region Command Methods
-
-        /// <inheritdoc/>
-        public async Task<User> CreateUserAsync(User user)
-        {
-            var existingUser = await GetUserByEmailAsync(user.EmailAddress);
-            if (existingUser != null)
-            {
-                throw new InvalidOperationException("A user with this email already exists");
-            }
-
-            await _userRepository.AddAsync(user);
-            return user;
-        }
-
-        /// <inheritdoc/>
-        public async Task<User> UpdateUserAsync(int id, User updatedUser)
-        {
-            var existingUser = await _userRepository.GetByIdAsync(id);
-            if (existingUser == null)
-            {
-                throw new KeyNotFoundException("User not found");
-            }
-
-            var userWithEmail = await GetUserByEmailAsync(updatedUser.EmailAddress);
-            if (userWithEmail != null && userWithEmail.Id != id)
-            {
-                throw new InvalidOperationException("A user with this email already exists");
-            }
-
-            existingUser.FirstName = updatedUser.FirstName;
-            existingUser.LastName = updatedUser.LastName;
-            existingUser.EmailAddress = updatedUser.EmailAddress;
-
-            await _userRepository.UpdateAsync(existingUser);
-            return existingUser;
-        }
-
-        /// <inheritdoc/>
-        public async Task DeleteUserAsync(int id)
-        {
-            var user = await _userRepository.GetByIdAsync(id);
-            if (user == null)
-            {
-                throw new KeyNotFoundException("User not found");
-            }
-
-            await _userRepository.RemoveAsync(user);
-        }
-
         #endregion
     }
 }
